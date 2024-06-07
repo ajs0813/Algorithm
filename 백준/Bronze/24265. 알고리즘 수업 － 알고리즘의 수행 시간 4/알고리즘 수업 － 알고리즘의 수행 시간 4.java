b141edf9eccd9e707830210f752420e7d9914a5b
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-06-07 11:22:08 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+
+       long n = sc.nextLong(); // 입력 크기 N
+
+        System.out.println(n*(n-1)/2);
+        System.out.println(2);
+    }
+}
