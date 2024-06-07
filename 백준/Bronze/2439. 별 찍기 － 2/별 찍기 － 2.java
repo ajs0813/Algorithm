b5f9b2c9da92fda3e7cfcdf0705d7030c6a0dd4d
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-06-07 11:29:29 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 340 ms, Memory: 18280 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        for (int i = 1; i<=N; i++){ // i = 1, i = 2 ...
+
+            for(int j =1; j<=N - i; j++){
+                System.out.print(" ");
+            }
+
+            for(int j = 1; j<=i; j++) { // j = 1~5
+                System.out.print("*");
+
+            }
+            System.out.println("");
+        }
+    }
+}
