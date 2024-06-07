3713256a595e69208a0caecb870dc8c9cf61947c
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-06-07 17:31:22 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 180 ms, Memory: 17684 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -14,18 +14,17 @@ public class Main {
             sum += arr[i];
         }
 
-
+        Arrays.sort(arr);
 
         for (int i = 0; i < 8; i++) {
             for (int j = i + 1; j < 9; j++) {
                 if (sum - arr[i] - arr[j] == 100) {
-                    arr[i] = 0;
-                    arr[j] = 0;
-                    Arrays.sort(arr);
 
-                    for (int k = 2; k < 9; k++) {
-
-                        System.out.println(arr[k]);
+                    for (int k = 0; k < 9; k++) {
+                        if (k != i && k != j) {
+                            System.out.println(arr[k]);
+                        }
+                       
                     }
                     return;
                 }
