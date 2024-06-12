75acfd31ff3171a95eaaae33be11a1ad7fcd8c4d
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-06-13 05:52:03 +0900

    [Bronze III] Title: 지각, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/10419. 지각/지각.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // 수업 시간
+        int t = sc.nextInt();
+
+        // 각 수업 시간에 대해 반복
+        for(int i = 0; i < t; i++){
+            int d = sc.nextInt();
+            int max = 0;
+
+            // 가능한 최대 지각 시간을 찾기 위한 반복문
+            for(int j = 0; j*j <= d-j; j++){
+                //조건을 만족하는 j값을 max에 저장
+                max = j;
+            }
+            System.out.println(max);
+        }
+    }
+}
+
