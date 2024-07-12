f9b777cdaa2ce44ccc79587d7696364e86163d5f
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-07-13 03:15:14 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 0.06 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int answer = 0;
+        int sum1 = 1;
+        int sum2 = 0;
+        for(int i = 0; i<num_list.length; i++){
+            sum1 *= num_list[i];
+            sum2 += num_list[i];
+        }
+        
+        if(sum1>Math.pow(sum2,2)){
+            answer = 0;
+        } else if(sum1 < Math.pow(sum2, 2)) {
+            answer = 1;
+        }
+        
+        return answer;
+    }
+}
