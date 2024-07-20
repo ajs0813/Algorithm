17b4f0144695b8354bacc30124e0945a87a5f04f
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-07-21 02:17:22 +0900

    [level 0] Title: 수 조작하기 2, Time: 1105.49 ms, Memory: 380 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public String solution(int[] numLog) {
+        HashMap<Integer, String> map = new HashMap<>();
+        map.put(1, "w");
+        map.put(-1, "s");
+        map.put(10, "d");
+        map.put(-10, "a");
+        String answer = "";
+        
+        for(int i = 1; i<numLog.length; i++){
+            answer += map.get(numLog[i] - numLog[i-1]);
+        }
+        
+        return answer;
+    }
+}
