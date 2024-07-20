3e08acde6383b3494345ed605815d54b4323b361
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-07-21 01:27:33 +0900

    [level 0] Title: 등차수열의 특정한 항만 더하기, Time: 0.05 ms, Memory: 73 MB -BaekjoonHub

---
+++ 프로그래머스/0/181931. 등차수열의 특정한 항만 더하기/등차수열의 특정한 항만 더하기.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Solution {
+    public int solution(int a, int d, boolean[] included) {
+        if(included.length == 1){
+            return a;
+        }
+        int answer = 0;
+        int[] arr = new int[included.length];
+        arr[0] = a;
+        for(int i = 1; i<arr.length; i++){
+            arr[i] += arr[i-1] + d;
+        }
+        
+        for(int j = 0; j<arr.length; j++){
+            if(included[j]){
+                answer += arr[j];
+            }
+        }
+        
+        return answer;
+    }
+}
