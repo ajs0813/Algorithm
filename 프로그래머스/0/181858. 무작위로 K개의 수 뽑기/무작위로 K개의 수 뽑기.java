bbd0ea40cbda37689b81018b72bdc9f66c653e5e
Ahn Jae Seoung <143467352+ajs0813@users.noreply.github.com>
2024-07-23 10:11:21 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 4.97 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -0,0 +1,26 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] arr, int k) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (set.size() < k) {
+                set.add(arr[i]);
+            }
+        }
+
+        int[] answer = new int[k];
+
+        Iterator<Integer> iterator = set.iterator();
+
+        for (int i = 0; i < k; i++) {
+            if (iterator.hasNext()) {
+                answer[i] = iterator.next();
+            } else {
+                answer[i] = -1;
+            }
+        }
+
+        return answer;
+    }
+}
