f17172e94e511eb5bd4a23ca34d84ef6b6c2198a
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2024-08-07 11:21:55 +0900

    [level 1] Title: 2016년, Time: 1.71 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -0,0 +1,10 @@
+import java.time.*;
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        LocalDate specificDate = LocalDate.of(2016, a, b);
+        DayOfWeek dayOfWeek = specificDate.getDayOfWeek();
+        answer = dayOfWeek.name().substring(0,3);
+        return answer;
+    }
+}
