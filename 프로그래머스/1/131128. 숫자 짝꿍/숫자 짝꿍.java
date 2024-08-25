7272235917bcecf69e25977345f7a75af80c00c3
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2024-08-26 00:55:12 +0900

    [level 1] Title: 숫자 짝꿍, Time: 47.67 ms, Memory: 131 MB -BaekjoonHub

---
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.java
@@ -0,0 +1,41 @@
+class Solution {
+    public String solution(String X, String Y) {
+        String answer = "";
+        int[] Xarr = new int[10];
+        int[] Yarr = new int[10];
+
+        for (int i = 0; i < X.length(); i++) {
+            Xarr[X.charAt(i) - '0']++;
+        }
+
+        for (int i = 0; i < Y.length(); i++) {
+            Yarr[Y.charAt(i) - '0']++;
+        }
+
+        int[] same = new int[10];
+
+        for (int i = 0; i < same.length; i++) {
+            if(Xarr[i] != 0 && Yarr[i] != 0){
+                same[i] = Math.min(Xarr[i], Yarr[i]);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder(answer);
+
+        for (int i = same.length-1; i >= 0; i--) {
+           sb.append(Integer.toString(i).repeat(same[i]));
+        }
+
+        answer = sb.toString();
+
+        if(answer.isBlank()){
+            answer = "-1";
+        }
+
+        if(answer.startsWith("0")){
+            answer = "0";
+        }
+
+        return answer;
+    }
+}
