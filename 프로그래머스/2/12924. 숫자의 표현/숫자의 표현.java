c99a18681ef2f2c0223b89feae1b1bd4a08165fd
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2024-10-07 11:52:35 +0900

    [level 2] Title: 숫자의 표현, Time: 1.72 ms, Memory: 52.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+
+        for(int i = 1; i<=n; i++){
+            int sum = 0;
+            
+            // 현재 i부터 n까지 자연수를 더해가면서 합을 계산
+            for(int j = i; j <= n; j++){
+                sum += j;
+                
+                if(sum == n){
+                    answer++;
+                    break;
+                }
+                
+                // 합이 주어진 n보다 크면 내부 반복문 종료
+                if(sum > n) break;
+            }
+        }
+
+        return answer;
+    }
+}
