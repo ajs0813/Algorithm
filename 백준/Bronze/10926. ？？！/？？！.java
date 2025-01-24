a24c87e1b8c86554419f45b820a9bc57d48f2a57
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2025-01-24 21:26:23 +0900

    [Bronze V] Title: ??!, Time: 180 ms, Memory: 17808 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String id = scanner.nextLine(); // 아이디 입력
+        System.out.println(id + "??!"); // 놀람 표현 출력
+    }
+}
