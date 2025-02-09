ce4728c349120be7a2652535421443b87b7ca673
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2025-02-09 18:45:52 +0900

    [Bronze V] Title: A+B - 3, Time: 184 ms, Memory: 17956 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스 개수 입력 받기
+
+        for (int i = 0; i < T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A + B); // 각 줄마다 A + B 출력
+        }
+
+        sc.close(); // 스캐너 닫기
+    }
+}
