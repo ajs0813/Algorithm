6cd3004e28357adf862bae4e0482a32285475cde
Ahn Jae Seong <143467352+ajs0813@users.noreply.github.com>
2025-02-13 20:26:08 +0900

    [Bronze V] Title: A+B - 7, Time: 208 ms, Memory: 18792 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();  // 테스트 케이스 개수 입력
+
+        for (int i = 1; i <= T; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println("Case #" + i + ": " + (A + B));
+        }
+
+        sc.close();
+    }
+}
